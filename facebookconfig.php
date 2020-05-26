2ee9ae5eec67e5fd495509179d58ad8e6667c9c7
Axel Reuterskiöld <axel.reuterskiold@gmail.com>
2020-05-26 11:12:18 +0200

    n

--- facebookconfig.php
+++ facebookconfig.php
@@ -1,4 +1,5 @@
 <?php
-FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-});
+
+define('FB_APP_ID', '3051018188348914');
+define('FB_APP_SECRET', 'aecc3b100b8ba65079996d97db766f5b');
+define('FB_REDIRECT', 'http://localhost/login.php');
